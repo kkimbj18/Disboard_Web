150fe160519935264bf46361efcee63a4f4e2920
kkimbj18 <kkimg3802@gmail.com>
2021-05-31 22:00:41 +0900

    https적용 시 CI/CD 확인

--- Backend/config/swagger.js
+++ Backend/config/swagger.js
@@ -7,7 +7,7 @@ const doc = {
     },
     host: process.env.IP,
     basePath: "/api",
-    schemes: ['http'],
+    schemes: ['http', 'https'],
     tags: [
         { "name": "Auth" },
         { "name": "User" },
